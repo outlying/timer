4d9eed3cc632af5c97d27abaad68afad48946c46
Iwo Polański <iwo.polanski@schibsted.com>
2020-05-22 14:12:00 +0200

    Use interface

--- app-core/src/main/java/com/antyzero/timer/app/core/TimerCore.kt
+++ app-core/src/main/java/com/antyzero/timer/app/core/TimerCore.kt
@@ -1,17 +1,18 @@
 package com.antyzero.timer.app.core
 
 import com.antyzero.timer.app.core.timer.StandardTimer
+import com.antyzero.timer.app.core.timer.Timer
 
 class TimerCore(
     private val timeProvider: TimeProvider
 ) {
-    private val timers: MutableSet<StandardTimer> = mutableSetOf()
+    private val timers: MutableSet<Timer> = mutableSetOf()
 
-    fun registerNewTimer(timerTimeInSeconds: Long): StandardTimer {
+    fun createTimer(timerTimeInSeconds: Long): Timer {
         return StandardTimer(timeProvider, timerTimeInSeconds).also { timers }
     }
 
-    fun deregisterTimer(timer: StandardTimer) {
+    fun deregisterTimer(timer: Timer) {
         timers.remove(timer)
         // TODO should we destroy it ?
     }
