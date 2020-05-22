70fd71b3ef25b406939a4d8f93b17b58b12c74ca
Iwo Polański <iwo.polanski@schibsted.com>
2020-05-22 16:38:03 +0200

    Fix state recorder

--- app-core/src/test/kotlin/com/antyzero/timer/app/core/timer/utils/StateRecorder.kt
+++ app-core/src/test/kotlin/com/antyzero/timer/app/core/timer/utils/StateRecorder.kt
@@ -1,15 +1,18 @@
 package com.antyzero.timer.app.core.timer.utils
 
+import com.antyzero.timer.app.core.timer.State
+import com.antyzero.timer.app.core.timer.Timer
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 
 class StateRecorder(timer: Timer) {
 
-    private val _list: MutableList<Timer.State> = mutableListOf()
+    private val _list: MutableList<State> = mutableListOf()
     private val scope = CoroutineScope(Dispatchers.Unconfined)
 
-    val list: List<Timer.State>
+    val list: List<State>
         get() = _list
 
     init {
